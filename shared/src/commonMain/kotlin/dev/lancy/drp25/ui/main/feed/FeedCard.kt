a5f49851be16ce467a6b2c88d7fedf2991ff4538
Lancelot Liu <me@lancy.dev>
2025-05-29 21:40:26 +0100

    feat: Skeletons for feed.

---
+++ shared/src/commonMain/kotlin/dev/lancy/drp25/ui/main/feed/FeedCard.kt
@@ -0,0 +1,27 @@
+package dev.lancy.drp25.ui.main.feed
+
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import com.bumble.appyx.components.backstack.BackStack
+import com.bumble.appyx.components.backstack.BackStackModel
+import com.bumble.appyx.components.spotlight.Spotlight
+import com.bumble.appyx.components.spotlight.ui.fader.SpotlightFader
+import com.bumble.appyx.navigation.modality.NodeContext
+import com.bumble.appyx.navigation.node.LeafNode
+import dev.lancy.drp25.ui.main.MainNode.MainTarget
+import dev.lancy.drp25.utilities.Shape
+
+class FeedCard(
+    nodeContext: NodeContext,
+) : LeafNode(nodeContext) {
+    @Composable
+    override fun Content(modifier: Modifier) {
+        Box(modifier.clip(Shape.RoundedLarge)) {
+
+        }
+    }
+}
--- shared/src/commonMain/kotlin/dev/lancy/drp25/ui/main/feed/FeedNode.kt
+++ shared/src/commonMain/kotlin/dev/lancy/drp25/ui/main/feed/FeedNode.kt
@@ -2,14 +2,36 @@ package dev.lancy.drp25.ui.main.feed
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import com.bumble.appyx.components.backstack.ui.stack3d.BackStack3D
+import com.bumble.appyx.components.spotlight.Spotlight
+import com.bumble.appyx.components.spotlight.SpotlightModel
+import com.bumble.appyx.components.spotlight.ui.sliderscale.SpotlightSliderScale
+import com.bumble.appyx.navigation.composable.AppyxNavigationContainer
 import com.bumble.appyx.navigation.modality.NodeContext
 import com.bumble.appyx.navigation.node.LeafNode
 import dev.lancy.drp25.ui.main.MainNode
+import dev.lancy.drp25.ui.shared.NavConsumer
+import dev.lancy.drp25.ui.shared.NavConsumerImpl
 
-class FeedNode(nodeContext: NodeContext, parent: MainNode): LeafNode(nodeContext) {
+class FeedNode(
+    nodeContext: NodeContext,
+    parent: MainNode,
+    val spotlight: Spotlight<Unit> = Spotlight(
+        model = SpotlightModel(items = listOf(Unit), savedStateMap = mapOf()),
+        visualisation = { SpotlightSliderScale(
+            uiContext = it,
+            initialState = SpotlightModel.State(
+                positions = listOf(),
+                activeIndex = 0f,
+            )
+        )}
+    )
+): LeafNode(nodeContext),
+    NavConsumer<MainNode.MainTarget, MainNode> by NavConsumerImpl(parent) {
     @Composable
     override fun Content(modifier: Modifier) {
-        Text("Feed")
+        AppyxNavigationContainer(spotlight)
     }
 }
