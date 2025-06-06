a281ea3fbe1e1988b7e9c98ee89e0c38bbb3c318
LawanF <fathullahlawan@gmail.com>
2025-06-06 01:40:47 +0100

    feat: initial skeleton for filters page

--- shared/src/commonMain/kotlin/dev/lancy/drp25/ui/main/feed/FilterNode.kt
+++ shared/src/commonMain/kotlin/dev/lancy/drp25/ui/main/feed/FilterNode.kt
@@ -1,14 +1,22 @@
 package dev.lancy.drp25.ui.main.feed
 
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.RangeSlider
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Slider
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,37 +24,46 @@ import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.bumble.appyx.navigation.modality.NodeContext
 import com.bumble.appyx.navigation.node.LeafNode
+import com.composables.icons.lucide.Lucide
+import com.composables.icons.lucide.Search
 import dev.lancy.drp25.ui.main.MainNode
 import dev.lancy.drp25.utilities.DishCuisine
 import dev.lancy.drp25.utilities.DishDiet
 import dev.lancy.drp25.utilities.DishType
+import dev.lancy.drp25.utilities.Ingredients
 import dev.lancy.drp25.utilities.Shape
 import dev.lancy.drp25.utilities.Size
 import dev.lancy.drp25.utilities.Typography
 
 class FilterNode(nodeContext: NodeContext, parent: MainNode): LeafNode(nodeContext) {
-    private val TIME_SLIDER_RANGE = 0f..100f
+    private val PLACEHOLDER_SLIDER_RANGE = 0f..100f
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Content(modifier: Modifier) {
-        Column(modifier = Modifier.padding(Size.Padding)) {
+        Column(
+            modifier = Modifier
+            .padding(Size.Padding)
+            .verticalScroll(rememberScrollState())
+            .padding(bottom = Size.BarLarge)
+        ) {
             Text(
                 "Filters",
                 style = Typography.titleMedium
             )
 
             Text("Time")
-            var timeSliderPosition by remember { mutableStateOf(TIME_SLIDER_RANGE) }
+            var timeSliderPosition by remember { mutableStateOf(PLACEHOLDER_SLIDER_RANGE) }
             RangeSlider(
                 value = timeSliderPosition,
                 onValueChange = { timeSliderPosition = it },
-                valueRange = TIME_SLIDER_RANGE
+                valueRange = PLACEHOLDER_SLIDER_RANGE
             )
 
             Text("Rating")
@@ -123,6 +140,129 @@ class FilterNode(nodeContext: NodeContext, parent: MainNode): LeafNode(nodeConte
                     )
                 }
             }
+
+            Text("Must include")
+            var includeTextFieldState by remember { mutableStateOf("") }
+            var includeActive by remember { mutableStateOf(false) }
+            SearchBar(
+                query = includeTextFieldState,
+                onQueryChange = { includeTextFieldState = it },
+                onSearch = { includeActive = false },
+                content = {},
+                active = includeActive,
+                onActiveChange = {},
+                placeholder = {
+                    Row {
+                        Icon(imageVector = Lucide.Search, null, modifier = Modifier.padding(
+                            end = Size.Padding
+                        ))
+                        Text("Search ingredients", color = Color.LightGray)
+                    }
+                },
+            )
+            FlowRow {
+                Ingredients.entries.take(3).forEach { ingredient ->
+                    AssistChip(
+                        onClick = {},
+                        label = {
+                            androidx.compose.material.Text(
+                                ingredient.displayName,
+                                style = Typography.bodySmall
+                            )
+                        },
+                        modifier = Modifier.defaultMinSize(minHeight = 28.dp),
+                        colors = AssistChipDefaults.assistChipColors(
+                            containerColor = Color.LightGray,
+                            labelColor = Color.Black,
+                        ),
+                        shape = Shape.RoundedLarge,
+                        elevation = AssistChipDefaults.assistChipElevation(),
+                    )
+                }
+            }
+
+            Text("Must avoid")
+            var avoidTextFieldState by remember { mutableStateOf("") }
+            var avoidActive by remember { mutableStateOf(false) }
+            SearchBar(
+                query = avoidTextFieldState,
+                onQueryChange = { avoidTextFieldState = it },
+                onSearch = { avoidActive = false },
+                content = {},
+                active = avoidActive,
+                onActiveChange = {},
+                placeholder = {
+                    Row {
+                        Icon(imageVector = Lucide.Search, null, modifier = Modifier.padding(
+                            end = Size.Padding
+                        ))
+                        Text("Search ingredients", color = Color.LightGray)
+                    }
+                },
+            )
+            FlowRow {
+                Ingredients.entries.take(3).forEach { ingredient ->
+                    AssistChip(
+                        onClick = {},
+                        label = {
+                            androidx.compose.material.Text(
+                                ingredient.displayName,
+                                style = Typography.bodySmall
+                            )
+                        },
+                        modifier = Modifier.defaultMinSize(minHeight = 28.dp),
+                        colors = AssistChipDefaults.assistChipColors(
+                            containerColor = Color.LightGray,
+                            labelColor = Color.Black,
+                        ),
+                        shape = Shape.RoundedLarge,
+                        elevation = AssistChipDefaults.assistChipElevation(),
+                    )
+                }
+            }
+
+            Row {
+                Text("Only use my equipment")
+                var equipmentChecked by remember { mutableStateOf(true) }
+                Switch(
+                    checked = equipmentChecked,
+                    onCheckedChange = {
+                        equipmentChecked = it
+                    },
+                )
+            }
+
+            Text("Calories")
+            var calorieSliderPosition by remember { mutableStateOf(PLACEHOLDER_SLIDER_RANGE) }
+            RangeSlider(
+                value = calorieSliderPosition,
+                onValueChange = { calorieSliderPosition = it },
+                valueRange = PLACEHOLDER_SLIDER_RANGE
+            )
+
+            Text("Protein")
+            var proteinSliderPosition by remember { mutableStateOf(PLACEHOLDER_SLIDER_RANGE) }
+            RangeSlider(
+                value = proteinSliderPosition,
+                onValueChange = { proteinSliderPosition = it },
+                valueRange = PLACEHOLDER_SLIDER_RANGE
+            )
+
+            Text("Fat")
+            var fatSliderPosition by remember { mutableStateOf(PLACEHOLDER_SLIDER_RANGE) }
+            RangeSlider(
+                value = fatSliderPosition,
+                onValueChange = { fatSliderPosition = it },
+                valueRange = PLACEHOLDER_SLIDER_RANGE
+            )
+
+            Text("Carbohydrates")
+            var carbsSliderPosition by remember { mutableStateOf(PLACEHOLDER_SLIDER_RANGE) }
+            RangeSlider(
+                value = carbsSliderPosition,
+                onValueChange = { carbsSliderPosition = it },
+                valueRange = PLACEHOLDER_SLIDER_RANGE
+            )
 
         }
 
